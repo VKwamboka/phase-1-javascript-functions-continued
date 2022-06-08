ec5682a39fa2625796c58a381f103d54b70263cc
verah <verahnyagaka101@gmail.com>
2022-06-08 15:58:53 +0300

    commit tree 775e23af3da84bd130bbe1890295b8a4d60fce81

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+// let wrapAdjective = function(style="*") {
+//     return function(word="special") {
+//       return `You are ${style}${word}${style}!`
+//     }
+//   }
+
+const saturdayFun = function (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  };
+  
+  const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+   };
+//    mondayWork();
+const wrapAdjective = function (special = "*") {
+    return function (adjective = amazing){
+        return `You are ${special}${adjective}${special}!`
+    }
+   };
+
+//    function wrapAdjective (greeting, msg = "It's a fine day to learn") {
+//     const innerFunction = function (name, lang = "Python") {
+//       return `${greeting}, ${name}! ${msg} ${lang}`;
+//     };
+//     return innerFunction;
+//   }
