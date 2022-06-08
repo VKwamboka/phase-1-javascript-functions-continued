6b80b63a7a0f26c7c84af28117154f3201e5af42
verah <verahnyagaka101@gmail.com>
2022-06-08 15:59:58 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const saturdayFun = function (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  };
+  
+  const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+   };
+//    mondayWork();
+
+const wrapAdjective = function (special = "*") {
+    return function (adjective = amazing){
+        return `You are ${special}${adjective}${special}!`
+    }
+   };
+
